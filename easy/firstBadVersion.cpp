16068b9a1a2a8f0c1ede19f5a7c692c0d88da2b4
amavg2002 <amavg2002@gmail.com>
2021-09-13 03:23:49 -0700

    Implement Binary search to find the first bad version in O(logn) time

---
+++ easy/firstBadVersion.cpp
@@ -0,0 +1,12 @@
+
+int firstBadVersion(int n)
+{
+	int start=1, end=n;
+	while(start<=n)
+	{
+		int mid=start+(end-start)/2;
+		if(isBadVersion(mid)) end=mid;
+		else start=mid+1;
+	}
+	return end;
+}
